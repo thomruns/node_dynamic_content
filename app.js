369e9e5d602019573b0c385401c6d4173e6a28e3
Thom David <thomdavid74@gmail.com>
2019-04-05 15:00:21 -0700

    Pugify remaining html pages

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/admin', adminObj.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404');
 });
 
 app.listen(port);
