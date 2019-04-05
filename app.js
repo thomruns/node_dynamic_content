4fe2a81355b392490379cae2d00ad60e6f6dd5c2
Thom David <thomdavid74@gmail.com>
2019-04-05 15:39:34 -0700

    Use pug layout features

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/admin', adminObj.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { pageTitle: '404 - Not Found' });
 });
 
 app.listen(port);
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminObj.products; // to use in pug template
-  res.render('shop', { prods: products, docTitle: 'Shop' }); // looks for pug template, data to pass
+  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' }); // looks for pug template, data to pass
 });
 
 module.exports = router;
