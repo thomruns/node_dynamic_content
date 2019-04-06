899f151b339c740f50655726b6959f7fe5abecd3
Thom David <thomdavid74@gmail.com>
2019-04-06 11:40:59 -0700

    Initial ejs setup

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,6 @@ const adminObj = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log('shop.js', adminObj.products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
