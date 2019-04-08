f57c4a924bb5ed3cd1c4f620f0022343fe6297ea
Thom David <thomdavid74@gmail.com>
2019-04-08 09:26:28 -0700

    Code comments pre-MVC refactor

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const path = require('path');
+const path = require('path'); // node core module
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-// add view as the view engine and set the views path
+// add ejs as the view engine and set the views path
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -13,12 +13,13 @@ const port = 3000;
 
 // this is now exported as an object since additional properties 
 // are being exported by the admin module
-const adminObj = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
+const adminObj = require('./routes/admin');  //exported object
+const shopRoutes = require('./routes/shop'); // exported function
+// use body parser
 app.use(bodyParser.urlencoded({extended: false}));
+// base directory path for all static files
 app.use(express.static(path.join(__dirname, 'public')));
-
+// use these routes
 app.use('/admin', adminObj.routes);
 app.use(shopRoutes);
 
